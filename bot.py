a03e02c7d3a1e1476b624bfd332aa46d3900204f
2020mtran <2020mtran@gmail.com>
2024-06-04 19:39:56 -0700

    added riot api key

--- bot.py
+++ bot.py
@@ -1,16 +1,32 @@
-# This example requires the 'message_content' intent.
-
 import discord
+import logging
+import requests
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+# Assume client refers to a discord.Client subclass...
+client.run(token, log_handler=handler, log_level=logging.DEBUG)
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run('my token goes here')
+client = discord.Client(intents=intents)
+
+api_key = "RGAPI-747a3755-9640-41c5-addd-be2e137ec919"
+api_url = ""
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    
+
+
+client.run('MTI0NzM1NTM1NTczNDg2ODAwOQ.GtGp3R.x7qhULV-q4UWM0bYj-s-YEMZVq0_u9MAYVoQCA')
